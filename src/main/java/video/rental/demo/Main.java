e0e78a7934cfa2161571e779970516d4bc948625
Kim Heeseok <zxcasd12@gmail.com>
2021-04-07 16:02:13 +0900

    Refactoring #1

--- src/main/java/video/rental/demo/Main.java
+++ src/main/java/video/rental/demo/Main.java
@@ -1,13 +1,24 @@
 package video.rental.demo;
 
+import video.rental.demo.application.Interactor;
+import video.rental.demo.domain.Repository;
+import video.rental.demo.infrastructure.RepositoryDBImpl;
 import video.rental.demo.presentation.CmdUI;
+import video.rental.demo.utils.SampleGenerator;
 
 public class Main {
 	private static CmdUI ui;
 
 	public static void main(String[] args) {
-//		Repository repository = new RepositoryDBImpl();
-//		ui = new CmdUI(repository);
-//		ui.start();
+		Repository repository = new RepositoryDBImpl();
+		new SampleGenerator(repository).generateSamples();
+
+		Interactor interactor = new Interactor(repository);
+
+		ui = new CmdUI(interactor);
+		ui.start();
+
+//		GraphicUI gui = new GraphicUI();
+//		gui.start();
 	}
 }
---
+++ src/main/java/video/rental/demo/domain/prices/Price.java
@@ -0,0 +1,11 @@
+package video.rental.demo.domain.prices;
+
+public abstract class Price {
+    public abstract int getPriceCode();
+
+    public abstract double getCharge(int daysRented);
+
+    public int getSavingPoint() {
+        return 1;
+    }
+}
